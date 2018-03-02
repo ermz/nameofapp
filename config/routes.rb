ac0238995cbdc3bd54057fb882b90452bc58556d
Edson Ramirez <eramirez4369@gmail.com>
2018-03-02 13:32:55 -0500

    3.11 change website theme and add styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'simple_pages/index' =>"simple_pages#index"
+  get 'simple_pages/index', to: 'simple_pages#index', as: 'index'
 
-  get 'simple_pages/about'
+  get 'simple_pages/about', to: 'simple_pages#about', as: 'about'
 
-  get 'simple_pages/contact'
+  get 'simple_pages/contact', to: 'simple_pages#contact', as: 'contact'
 
   root 'simple_pages#index'
 
