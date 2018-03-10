3a013b6b44f84c9d02c6a47d843493e0e640f73b
Edson Ramirez <eramirez4369@gmail.com>
2018-03-10 01:01:16 -0500

    created orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/index', to: 'simple_pages#index', as: 'index'
 
   get 'simple_pages/about', to: 'simple_pages#about', as: 'about'
 
   get 'simple_pages/contact', to: 'simple_pages#contact', as: 'contact'
 
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
