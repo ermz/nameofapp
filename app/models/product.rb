22687182b35545fa757b260162a4819f3adccdb2
Edson Ramirez <eramirez4369@gmail.com>
2018-03-28 01:09:35 -0400

    Added bootstrap to forms and added search bar in header

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   has_many :orders
+
+  def self.search(search_term)
+    matching_operator = Rails.env.development? ? "LIKE" : "ilike"
+    Product.where("name #{matching_operator} ?", "%#{search_term}")
+  end
 end
