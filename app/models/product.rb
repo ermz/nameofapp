9d35ebed58a928fcff978e91c6432eb1570dcd90
Edson Ramirez <eramirez4369@gmail.com>
2018-03-28 18:33:58 -0400

    5.2 fixed self.search method in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,11 @@ class Product < ApplicationRecord
   has_many :orders
 
   def self.search(search_term)
-    matching_operator = Rails.env.development? ? "LIKE" : "ilike"
-    Product.where("name #{matching_operator} ?", "%#{search_term}")
+    if Rails.env.production?
+      Product.where("name ilike?", "%#{search_term}%")
+    else
+      Product.where("name LIKE ?", "%#{search_term}%")
+    end
   end
+  
 end
