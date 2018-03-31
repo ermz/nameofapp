45a3c861b8a489e9d7a138033f6439406730f841
Edson Ramirez <eramirez4369@gmail.com>
2018-03-31 01:51:02 -0400

    4.7 made small changes to carousel

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,6 +2,16 @@ class SimplePagesController < ApplicationController
   def index
   end
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: 'eramirez4369@gmail.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
+
   def landing_page
     @products = Product.limit(3)
     @data_slide_num = 0
