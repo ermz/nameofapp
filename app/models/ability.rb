c76c4a99bf520b7ab051883c6744ec5cfdf2842a
Edson Ramirez <eramirez4369@gmail.com>
2018-04-18 17:29:12 -0400

    5.10 added admin flag and restricted delete functionality

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,5 +4,12 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    can :read, :all .
+    if user.present?
+      can :manage, Post, id: user.id
+      if user.admin?
+        can :manage, :all
+      end
+    end
   end
 end
