26a88e8dc5f43c23e20fb7d34ae1ba09c9097add
Edson Ramirez <eramirez4369@gmail.com>
2018-05-07 15:55:35 -0400

    6.6 made changes to payment controller, removed UserMailer action

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -14,7 +14,7 @@ class PaymentsController < ApplicationController
 
       if charge.paid
         Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
-        UserMailer.place_order(@user, @product).deliver_now
+        #UserMailer.place_order(@user, @product).deliver_now
       end
 
     rescue Stripe::CardError => e
@@ -25,6 +25,5 @@ class PaymentsController < ApplicationController
     end
     redirect_to product_path(@product)
   end
-
 
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,18 +8,11 @@ class UserMailer < ApplicationMailer
            subject: "A new contact form message from #{name}")
   end
 
-  def welcome_new_user(user)
-    @user = user
-    mail(from: email,
-         to: user.email,
-         subject: "Thank you for signing up!")
-  end
-
   def place_order(user, product)
     @user = user
     @product = product
     mail(from: "eramirez4369@gmail.com",
-         to: "eramirez4369@gmail.com",
-         subject: "Thank you, for your order. Below is an order confirmation for product name.")
+         to: user.email,
+         subject: "Thank you, for your order. Below is an order confirmation for #{product.name}.")
   end
 end
